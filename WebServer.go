20072043578d55fc8997ff0fd1df5197574b1249
Shetty Roshith Raghurama <121103694+07Roshi@users.noreply.github.com>
2023-09-01 03:51:35 +0000

    update

--- WebServer.go
+++ WebServer.go
@@ -2,12 +2,13 @@ package main
 import (
     "fmt"
     "net/http"
+    "log"
 )
 
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Hello, Friends!")
     })
-
+    log.Println("Server listening on port 8080....")
     http.ListenAndServe(":8080", nil)
 }
